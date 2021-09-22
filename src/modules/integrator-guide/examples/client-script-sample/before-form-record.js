275522a8d08ea79f550fb91ef8e06387f5e78853
Denis Arh <denis.arh@gmail.com>
2021-09-22 12:19:04 +0200

    Client script examples fixed

--- src/modules/integrator-guide/examples/client-script-sample/before-form-record.js
+++ src/modules/integrator-guide/examples/client-script-sample/before-form-record.js
@@ -4,14 +4,23 @@ export default {
 
   triggers ({ before }) {
     return before('formSubmit')
-      .for('compose:record')
-      .where('module', 'module goes here')
-      .where('namespace', 'namespace goes here')
+      .for('ui:compose:record-page')
   },
 
   // If you don't need the Compose helper, remove it
-  async exec ({ $record }, { Compose }) {
-    // Code goes here
+  async exec ({ $record }, { Compose, console }) {
     // Note: you don't need to return the $record; it's provided by reference
+    //       so any changes you do to the original object will be directly applied
+    //
+
+    if (['crm'].includes($record.module.namespace.slug.toLowerCase())) {
+      // interested only in CRM namespace
+      return
+    }
+
+    if (['lead'].includes($record.module.handle.toLowerCase())) {
+      // interested only in Lead module
+      return
+    }
   },
 }
